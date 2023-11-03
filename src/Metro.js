1029a9e4dc85f033a65054140695415cfab73784
AndresGArcia <andrga37@ucm.es>
2023-11-03 11:54:45 +0100

    metro arreglado

--- src/Metro.js
+++ src/Metro.js
@@ -7,40 +7,72 @@ export default class Metro extends Phaser.Scene {
         super({ key: 'Metro', active: false }); 
     }
 
-    // Métodos init, preload, create, update
+    // Métodos init, preload, create, update+
+    init(){
+        this.MovementY = 0;
+        this.MovementX = 0;
+        this.speed = 100;
+        this.rozamiento = 101;
+    }
     preload(){        
-		this.load.image("beluga", "/assets/juego/TruthOrDare/imagenes/Beluga2.png");
-        this.load.image('atras', '/assets/juego/TruthOrDare/imagenes/VolverAtras.jpg'); // Cargamos la imagen de volver atras (provisional).
+		this.load.image("beluga", "assets/juego/TruthOrDare/imagenes/Beluga2.png");
+        this.load.image('atras', "assets/juego/TruthOrDare/imagenes/VolverAtras.jpg"); // Cargamos la imagen de volver atras (provisional).
     }    
     create(){               
         this.avion = this.physics.add.image(540, 360, "beluga").setScale(.25)
         
         this.atras = this.add.image(0, 0, 'atras').setOrigin(0, 0).setScale(0.1, 0.1).setInteractive(); // Añadimos la imagen de volver atras.
         this.atras.on('pointerdown', (pointer) => {
-            finalDelJuego()
+            this.finalDelJuego()
         });
 
         this.input.keyboard.on('keydown-W', event =>
         {
-            this.moverArriba()
+            this.MovementY = -this.speed;
 
         });
         this.input.keyboard.on('keydown-S', event =>
         {
-            this.moverAbajo()
+            this.MovementY = this.speed;
+        });
+        this.input.keyboard.on('keydown-A', event =>
+        {
+            this.MovementX = -this.speed;
+
+        });
+        this.input.keyboard.on('keydown-D', event =>
+        {
+            this.MovementX = this.speed;
         });
     }
 
-    moverArriba(){
-        this.avion.y -= 10
-    }
-    moverAbajo(){
-        this.avion.y += 10
-    }
+    update(){
+        this.moverAvion()
+        
+        if( this.MovementX > 1){
+            this.MovementX -= this.rozamiento;
+        }        
+        else if (this.MovementX < -1){
+            this.MovementX += this.rozamiento;
+        }
+        else this.MovementX = 0;
 
+        if( this.MovementY > 1){
+            this.MovementY -= this.rozamiento;
+        }
+        else if(this.MovementY < -1){
+            this.MovementY += this.rozamiento;
+        }
+        else this.MovementY = 0;
+    }
     finalDelJuego()
     {    
         this.scene.start("Hub");
     }
+
+    moverAvion(){
+        this.avion.setAcceleration(this.MovementX,this.MovementY)
+    }
+
     
    } 
