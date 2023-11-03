6f417a273a2cb845605673e552a151fdd45681a4
AndresGArcia <andrga37@ucm.es>
2023-11-03 02:01:14 +0100

    no se ven las imagenes :'(

--- src/hub.js
+++ src/hub.js
@@ -7,26 +7,26 @@ export default class Hub extends Phaser.Scene{
     }
     
     preload(){
-      this.load.image('ToDImage', "/assets/juego/Hub/TruthOrDare.jpg")
-      this.load.image('BumClakImage', "/assets/juego/Hub/BumClack.jpg")
-      this.load.image('PapasImage', "/assets/juego/Hub/Papas.jpg")
-      this.load.image('Metro', "/assets/juego/Hub/Metro.jpeg")
+      this.load.image('ToDImage', "assets/Bocetos/TruthOrDare.PNG")
+      this.load.image('BumClakImage', "assets/Bocetos/BumBumKlak.PNG")
+      this.load.image('PapasImage', "assets/Bocetos/PappasGalleteria.PNG")
+      this.load.image('Metro', "assets/Bocetos/MetroSkaters.PNG")
     }
 
     create(){               
-      this.ToDImg = this.add.image(0, 0, 'ToDImage').setOrigin(0,0).setScale(0.75,0.25)
+      this.ToDImg = this.add.image(0, 0, 'ToDImage').setOrigin(0,0).setScale(1,0.71)
       this.ToDImg.setInteractive().on('pointerdown', () => 
       this.scene.start("VerdadOReto")
       )
-      this.BumClackImg = this.add.image(500, 0, 'BumClakImage').setOrigin(0,0).setScale(0.5)
+      this.BumClackImg = this.add.image(500, 0, 'BumClakImage').setOrigin(0,0).setScale(.8)
       this.BumClackImg.setInteractive().on('pointerdown', () => 
       this.scene.start("BumKlak")
       )
-      this.PapasImg = this.add.image(0, 300, 'PapasImage').setOrigin(0,0).setScale(0.5)
+      this.PapasImg = this.add.image(0, 300, 'PapasImage').setOrigin(0,0).setScale(.8)
       this.PapasImg.setInteractive().on('pointerdown', () => 
       this.scene.start("Papas")
       )
-      this.PapasImg = this.add.image(500, 300, 'Metro').setOrigin(0,0).setScale(0.3)
+      this.PapasImg = this.add.image(500, 300, 'Metro').setOrigin(0,0).setScale(.8)
       this.PapasImg.setInteractive().on('pointerdown', () => 
       this.scene.start("Metro")
       )
