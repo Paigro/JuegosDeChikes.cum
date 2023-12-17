d44b8f0e0a8597d3a9792ecd7be85778bb78d03f
Mauro Martínez <maumar03@ucm.es>
2023-12-17 19:24:38 +0100

    test

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ import Hub from "./hub.js";
 import Metro from "./Metro/Metro.js";
 import TruthOrDare from "./VerdadReto.js";
 import Papas from "./Papas/papas.js";
-import BumBumClack from "./bumKlak.js";
+import BumBumClack from "./Bum Bum Klak/bumKlak.js";
 import AbogadoClick from "./Clickers/AbogadoClick.js";
 
 const config = {
