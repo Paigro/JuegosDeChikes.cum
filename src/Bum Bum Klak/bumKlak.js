4117e0c22151884758063bc8701c82b70f848367
Mauro Martínez <maumar03@ucm.es>
2023-12-18 20:28:09 +0100

    assets remasterizados y fondo basico

--- src/Bum Bum Klak/bumKlak.js
+++ src/Bum Bum Klak/bumKlak.js
@@ -12,15 +12,19 @@ export default class BumKlak extends Phaser.Scene{
 
     preload(){        
       this.load.image('atras', '/assets/juego/TruthOrDare/imagenes/VolverAtras.jpg'); // Cargamos la imagen de volver atras (provisional).
-      this.load.image('fondo', '/assets/juego/BumKlak/fondo.png'); // cargamos fondo
+      this.load.image('bocadillo', '/assets/juego/BumKlak/bocadillo.png'); // Cargamos la imagen del bocadillo
+      this.load.image('cerebro', '/assets/juego/BumKlak/cerebro.png'); // Cargamos la imagen del cerebro
+      this.load.image('corazon', '/assets/juego/BumKlak/corazon.png'); // Cargamos la imagen del corazón
+      this.load.image('escuchador', '/assets/juego/BumKlak/persona escuchadora.png'); // Cargamos la imagen del moñeco que escucha
+      this.load.image('parlanchin', '/assets/juego/BumKlak/persona parlanchina.png'); // Cargamos la imagen del moñeco que habla
     }    
     create(){
       this.atras = this.add.image(0, 0, 'atras').setOrigin(0, 0).setScale(0.1, 0.1).setInteractive(); // Añadimos la imagen de volver atras.
-      //this = this.add.image(0,0, 'fondo').setOrigin(0,0).setScale(1,1);
-      this.add.text(100, 200, 'Escena del BumBumClack',  { fill: '#0f0' });
       this.atras.on('pointerdown', (pointer) => {
         this.finalDelJuego();
       });
+      this.escuchador = this.add.image(50, 350, 'escuchador').setOrigin(0,0).setScale(1, 1);
+      this.escuchador = this.add.image(750, 350, 'parlanchin').setOrigin(0,0).setScale(0.3, 0.3);
     }
     finalDelJuego()
 {    
