a7906b95968c1c21955d10d23bb84b6d92783003
Paigro <pabigl04@ucm.es>
2023-12-20 02:22:26 +0100

    SuperSwitch.  :-1:
    Me estan llevando por el mal camino de hacer switchs  en vez de escaleras de if elses... :-1:

--- src/resultadoTest.js
+++ src/resultadoTest.js
@@ -9,48 +9,32 @@ export default class resultadoTest extends Phaser.Scene // Manager de la escena 
     // Metodos init, preload, create, update:
     init(data) {
         this.coor = data;
-
-
-
-
-
-        // Este color se lo pasara el coordinator dependiendo de la personalidad.
-        this.cameras.main.setBackgroundColor("#84E2FE");
-
-
-
-
-
-
-
-
     }
 
     preload() {
         // IMAGENES:
-        /*// Amarillo:
-        this.load.image('virtuoso', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('aventurero', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('emprendedor', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('animador', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
+        // Amarillo:
+        this.load.image('virtuoso', "/assets/juego/personalidades/AmarilloAnimador.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('aventurero', "/assets/juego/personalidades/AmarilloAventurero.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('emprendedor', "/assets/juego/personalidades/AmarilloEmprendedor.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('animador', "/assets/juego/personalidades/AmarilloAnimador.png"); // Cargamos la imagen de volver atras (provisional).
         // Morado:
-        this.load.image('arquitecto', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('logico', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('camandante', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('innovador', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('arquitecto', "/assets/juego/personalidades/MoradoArquitecto.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('logico', "/assets/juego/personalidades/MoradoLogico.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('comandante', "/assets/juego/personalidades/MoradoComandante.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('innovador', "/assets/juego/personalidades/MoradoInnovador.png"); // Cargamos la imagen de volver atras (provisional).
         // Verde:
-        this.load.image('abogado', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('mediador', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('protagonista', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('activista', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('abogado', "/assets/juego/personalidades/VerdeAbogado.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('mediador', "/assets/juego/personalidades/VerdeMediador.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('protagonista', "/assets/juego/personalidades/VerdeProtagonista.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('activista', "/assets/juego/personalidades/VerdeActivista.png"); // Cargamos la imagen de volver atras (provisional).
         // Azul:
-        this.load.image('logista', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('defensor', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('ejecutivo', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-        this.load.image('consul', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).*/
+        this.load.image('logista', "/assets/juego/personalidades/AzulLogista.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('defensor', "/assets/juego/personalidades/AzulDefensor.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('ejecutivo', "/assets/juego/personalidades/AzulEjecutivo.png"); // Cargamos la imagen de volver atras (provisional).
+        this.load.image('consul', "/assets/juego/personalidades/AzulConsul.png"); // Cargamos la imagen de volver atras (provisional).
         // Boton:
         this.load.image('atras', "/assets/juego/MetroSkaters/imagenes/teclaizquierda.png"); // Cargamos la imagen de volver atras (provisional).
-
     }
 
     create() {
@@ -69,26 +53,122 @@ export default class resultadoTest extends Phaser.Scene // Manager de la escena 
         //#endregion
         //#region imagenes
         // Amarillo:
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'aventurero').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'emprendedor').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'animador').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.virtuoso = this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.aventurero = this.add.sprite(0, 0, 'aventurero').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.emprendedor = this.add.sprite(0, 0, 'emprendedor').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.animador = this.add.sprite(0, 0, 'animador').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
         // Morado:
-        this.add.sprite(0, 0, 'arquitecto').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.arquitecto = this.add.sprite(0, 0, 'arquitecto').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.logico = this.add.sprite(0, 0, 'logico').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.comandante = this.add.sprite(0, 0, 'comandante').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.innovador = this.add.sprite(0, 0, 'innovador').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
         // Verde:
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.abogado = this.add.sprite(0, 0, 'abogado').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.mediador = this.add.sprite(0, 0, 'mediador').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.protagonista = this.add.sprite(0, 0, 'protagonista').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.activista = this.add.sprite(0, 0, 'activista').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
         // Azul:
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
-        this.add.sprite(0, 0, 'virtuoso').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.logista = this.add.sprite(0, 0, 'logista').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.defensor = this.add.sprite(0, 0, 'defensor').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.ejecutivo = this.add.sprite(0, 0, 'ejecutivo').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
+        this.consul = this.add.sprite(0, 0, 'consul').setOrigin(0, 0).setScale(0.3, 0.3); // Añadimos la imagen del fondo.
         //#endregion
+        //#region mostrar cositas
+        this.changeBackGroundColor(this.resultado);
+        this.showPersonality();
+        //#endregion
+    }
+
+    showPersonality(pers) {
+        switch (pers) {
+            // Amarillo:
+            case "IRED":
+                this.virtuoso.setVisible(true);
+                console.log("virtuoso");
+                break;
+            case "ISED":
+                this.aventurero.setVisible(true);
+                console.log("aventurero");
+                break;
+            case "ERED":
+                this.emprendedor.setVisible(true);
+                console.log("emprendedor");
+                break;
+            case "ESED":
+                this.animador.setVisible(true);
+                console.log("animador");
+                break;
+            // Morado:
+            case "IROG":
+                this.arquitecto.setVisible(true);
+                console.log("arquitecto");
+                break;
+            case "IREG":
+                this.logico.setVisible(true);
+                console.log("logico");
+                break;
+            case "EROG":
+                this.comandante.setVisible(true);
+                console.log("comandante");
+                break;
+            case "EREG":
+                this.innovador.setVisible(true);
+                console.log("innovador");
+                break;
+            // Verde:
+            case "ISOG":
+                this.abogado.setVisible(true);
+                console.log("abogado");
+                break;
+            case "ISEG":
+                this.mediador.setVisible(true);
+                console.log("mediador");
+                break;
+            case "EDOG":
+                this.protagonista.setVisible(true);
+                console.log("protagonista");
+                break;
+            case "ESEG":
+                this.activista.setVisible(true);
+                console.log("activista");
+                break;
+            // Azul:
+            case "IROD":
+                this.logista.setVisible(true);
+                console.log("logista");
+                break;
+            case "ISOD":
+                this.defensor.setVisible(true);
+                console.log("defensor");
+                break;
+            case "EROD":
+                this.ejecutivo.setVisible(true);
+                console.log("defensor");
+                break;
+            case "ESOD":
+                this.consul.setVisible(true);
+                console.log("consul");
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    changeBackGroundColor(pers) {
+        let color;
+        // Este color se lo pasara el coordinator dependiendo de la personalidad.
+        //Morado.
+        if (pers === "IROG" || pers === "IREG" || pers === "EROG" || pers === "EREG") { color = "#E16EDD"; }
+        //Verde.
+        else if (pers === "ISOG" || pers === "ISEG" || pers === "ESOG" || pers === "ESEG") { color = "#73E16E"; }
+        //Azul.
+        else if (pers === "IROD" || pers === "ISOD" || pers === "EROD" || pers === "ESOD") { color = "#77EAF5"; }
+        //Amarillo.
+        else if (pers === "IRED" || pers === "ISED" || pers === "ERED" || pers === "ESED") { color = "#FFC300"; }
+        //Naranja.
+        else { color = "#ff9933" }
+        this.cameras.main.setBackgroundColor(color);
     }
 
     finalDelJuego() {
